afc8d4e5801d08b671d99b0a23748533a7999c80
moemen gaballah <moemengaballa@gmail.com>
2023-04-18 22:40:23 +0200

    create first file binary data by proctcol buffer

---
+++ index2.js
@@ -0,0 +1,28 @@
+const Schema = require("./employees_pb");
+const fs = require("fs");
+
+const moemen = new Schema.Employee();
+moemen.setId(1);
+moemen.setName("Moemen");
+moemen.setSalary(1000);
+
+
+const aya = new Schema.Employee();
+aya.setId(2);
+aya.setName("Aya");
+aya.setSalary(2000);
+
+
+const rick = new Schema.Employee();
+rick.setId(2);
+rick.setName("rick");
+rick.setSalary(5000);
+
+const employees = new Schema.Employees();
+employees.addEmployees(moemen)
+employees.addEmployees(aya)
+employees.addEmployees(rick)
+
+const bytes = employees.serializeBinary();
+console.log("binary " + bytes);
+fs.writeFileSync("binarydata", bytes)
